0b05035697f864e5451518f0bf50533b2c2614d7
Carlos Sanz <carlos.sanzpenas@gmail.com>
2023-03-17 13:18:55 +0100

    refactor: :white_check_mark: modify test template

--- tests/test_template.py
+++ tests/test_template.py
@@ -4,14 +4,13 @@ import pytest
 def func(x):
     return x + 1
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def cucmserver():
     return cucm(
         cucmserver="192.168.80.230",
         username="ccmadministrator",
         password="NTTvoip2020.",
         version="12.5",
-
     )
 
 def test_sample():
